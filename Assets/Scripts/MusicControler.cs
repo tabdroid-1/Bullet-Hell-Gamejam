b818ae4c238c4909ecba52671a9a796cf6afabf2
tabby <tabdroid.official@gmail.com>
2022-04-25 01:55:24 +0400

    0.8 mac

---
+++ Assets/Scripts/MusicControler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicControler : MonoBehaviour
+{
+    public static MusicControler instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
